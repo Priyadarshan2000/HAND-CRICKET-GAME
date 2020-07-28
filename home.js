88a3188712adfe8c9982ff4633e37811b2cef3ce
Priyadarshan2000 <62868878+Priyadarshan2000@users.noreply.github.com>
2020-07-28 21:02:44 +0530

    Hand Cricket Game

---
+++ home.js
@@ -0,0 +1,118 @@
+let score = 0;
+let Tar = 0;
+let comScore = 0;
+let first = true;
+let ufirst = true;
+let out = false;
+let cwin = false;
+let pwin = false;
+let equal = false;
+function clickedMe(no){
+    let UserChoise = document.getElementById('UserChoise');
+    let up = document.getElementById('up');
+    let cp = document.getElementById('cp');
+    let Board = document.getElementById('scr');
+    let u = Math.floor(Math.random()*6);
+    let r = u + 1;
+    printBoard(up,no);
+    printBoard(cp,r);
+    UserChoise.innerHTML = no;
+    if(out==false){
+        if(ufirst==true){
+            ufirst = false;
+            Board.classList.add('upos');
+        }
+        AIfunc(no,r);
+    }
+    else{
+        if(first==true){
+            first = false;
+            Board.classList.remove('cen');
+            Board.classList.add('out');
+        }
+        bowling(no,r);
+    }    
+}
+function printBoard(p,no){
+    if(no == 1){
+        p.innerHTML = "☝️";
+    }
+    else if(no == 2){
+        p.innerHTML = "✌";
+    }
+    else if(no == 3){
+        p.innerHTML = "👌";
+    }
+    else if(no == 4){
+        p.innerHTML = "🖖";
+    }
+    else if(no == 5){
+        p.innerHTML = "🖐";
+    }
+    else{
+        p.innerHTML = "👍";
+    }
+}
+function AIfunc(uno,r){  
+    let ComChoise = document.getElementById('ComChoise'); 
+    let Score = document.getElementById('Score');
+    let target = document.getElementById('target');
+    let Board = document.getElementById('scr');
+    let batOrBall = document.getElementById('batOrBall');
+    ComChoise.innerHTML = r; 
+    if(uno!=r)
+    {
+        score = score + uno;
+        Score.innerHTML = score;
+    }
+    else{
+        let Board = document.getElementById('scr');
+        Board.classList.remove('upos');
+        Board.classList.add('cen');
+        Board.classList.add('red-glow');
+        Board.innerHTML="You are out ☝!!!";
+        out = true;
+        let s = score + 1;
+        Tar = s;
+        target.innerHTML = "target :" + s;
+        batOrBall.innerHTML = "You are bowling!!!";
+    } 
+}
+
+function bowling(uno,r){
+    let ComChoise = document.getElementById('ComChoise');
+    let target = document.getElementById('target');
+    let Board = document.getElementById('scr');
+    ComChoise.innerHTML = r;
+    comScore = comScore + r;
+    Board.classList.remove('red-glow');
+    Board.innerHTML = "Score :" + comScore;
+    if(cwin == false && pwin == false)
+    {
+        if(comScore > Tar){
+            cwin = true;
+            Board.classList.remove('out');
+            Board.classList.add('cen');
+            Board.classList.add('red-glow');
+            Board.innerHTML = "Computer win 😭!!!";
+        }
+        else if(comScore == Tar){
+            equal = true;
+        }
+    }
+    if(uno == r){
+        if(equal == false){
+            pwin = true;
+            Board.classList.remove('out');
+            Board.classList.add('cen');
+            Board.classList.add('green-glow');
+            Board.innerHTML = "Player win 😍!!!";
+        }
+        else{
+            Board.classList.remove('out');
+            Board.classList.add('cen');
+            Board.classList.add('yellow-glow');
+            Board.innerHTML = "The score are equal 😐";
+        }    
+    } 
+}
